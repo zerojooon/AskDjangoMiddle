bf653fa6beb597143b0fa38a80d7a4d2de11d735
zerojooon <jun041577@snu.ac.kr>
2020-07-27 16:52:36 +0900

    5강

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,6 +6,7 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
+    path('signup/', views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('profile/', views.profile, name='profile'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-# Create your views here.
+from django.conf import settings
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView
+from django.contrib.auth.models import User
 
 @login_required
 def profile(request):
     # request.user
     return render(request, 'accounts/profile.html')
+
+'''
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect(settings.LOGIN_URL)
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/signup/html',{
+        'form':form
+    })
+'''
+
+signup = CreateView.as_view(model= User, form_class = UserCreationForm,
+                            template_name='accounts/signup.html',
+                            success_url=settings.LOGIN_URL)
