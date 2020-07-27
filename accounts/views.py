491f17e1d4f4d762181cdb8895096829701b33ba
zerojooon <jun041577@snu.ac.kr>
2020-07-27 17:34:11 +0900

    7강

--- accounts/views.py
+++ accounts/views.py
@@ -1,29 +1,45 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, resolve_url
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
 from django.contrib.auth.models import User
+from django.contrib.auth import login as auth_login
 
 @login_required
 def profile(request):
     # request.user
     return render(request, 'accounts/profile.html')
 
-'''
-def signup(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            return redirect(settings.LOGIN_URL)
-    else:
-        form = UserCreationForm()
-    return render(request, 'accounts/signup/html',{
-        'form':form
-    })
-'''
 
-signup = CreateView.as_view(model= User, form_class = UserCreationForm,
-                            template_name='accounts/signup.html',
-                            success_url=settings.LOGIN_URL)
+# def signup(request):
+#     if request.method == 'POST':
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             auth_login(request, user)
+#             return redirect('profile')
+#     else:
+#         form = UserCreationForm()
+#     return render(request, 'accounts/signup/html',{
+#         'form':form
+#     })
+
+
+# signup = CreateView.as_view(model= User, form_class = UserCreationForm,
+#                             template_name='accounts/signup.html',
+#                             success_url=settings.LOGIN_URL)
+
+class SignupView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    template_name = 'accounts/signup.html'
+
+    def get_success_url(self):
+        return resolve_url('profile')
+    def form_valid(self, form):
+        user = form.save()
+        auth_login(self.request, user)
+        return redirect(self.get_success_url())
+
+signup = SignupView.as_view()
