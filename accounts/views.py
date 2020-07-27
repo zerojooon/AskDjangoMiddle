06f2ca38e2743040b6801f56ec1899304d0bd173
zerojooon <jun041577@snu.ac.kr>
2020-07-27 17:42:22 +0900

    8강

--- accounts/views.py
+++ accounts/views.py
@@ -11,13 +11,14 @@ def profile(request):
     # request.user
     return render(request, 'accounts/profile.html')
 
-
+#
 # def signup(request):
 #     if request.method == 'POST':
 #         form = UserCreationForm(request.POST)
 #         if form.is_valid():
 #             user = form.save()
 #             auth_login(request, user)
+#             next_url = request.GET.get('next') or 'profile'
 #             return redirect('profile')
 #     else:
 #         form = UserCreationForm()
@@ -36,7 +37,8 @@ class SignupView(CreateView):
     template_name = 'accounts/signup.html'
 
     def get_success_url(self):
-        return resolve_url('profile')
+        next_url = self.request.GET.get('next') or 'profile'
+        return resolve_url(next_url)
     def form_valid(self, form):
         user = form.save()
         auth_login(self.request, user)
